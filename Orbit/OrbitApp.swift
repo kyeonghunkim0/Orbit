8467da2966b25bd0b41f62e56cda7526c34cc525
KyeongHun Kim <kimkhuna1@gmail.com>
2025-09-11 20:50:06 +0900

    Initial Commit

---
+++ Orbit/OrbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrbitApp.swift
+//  Orbit
+//
+//  Created by 김경훈 on 9/11/25.
+//
+
+import SwiftUI
+
+@main
+struct OrbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
